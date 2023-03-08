d23cccb394b841ff37cd0a700f0d14d9772b5074
unknown <pmj060268@gmail.com>
2023-03-08 11:04:05 +0900

    2023-03-08 박민지 헤더, 메인페이지 반응형 작업완료

--- front-end/full/src/page/Header.js
+++ front-end/full/src/page/Header.js
@@ -1,6 +1,7 @@
 
 import React from 'react'
 import '../page/font/font.css'
+import '../page/Header.css'
 
 
 const Header = () => {
@@ -15,57 +16,25 @@ const Header = () => {
 
 
     return (
-        <header style={{
-            display: "flex",
-            maxWidth: "100%",
-            alignItems: "center",
-            justifyContent: "space-between",
-            height: "15vh",
-            backgroundColor: "#F7F7D1",
-            fontFamily: "BMJUA_ttf",
-            fontSize: "30px",
-            textAlign: "center",
-            color: "#3B6EAE"
-        }}>
+        <header className='header'>
             
             <h1 style={{ marginLeft: "2%" }}>댕냥이</h1>
             <nav>
-                <ul style={{
-                    display: "flex",
-                    // listStyle: "none",
-                    // float: "left",
-                    fontFamily: "BMJUA_ttf",
-                    fontSize: "40px",
-                    color: "#B5D0E5",
-                    
-                }}>
-
-                    <h1> Dog </h1>
-                    <h1 style={{marginLeft:"50%"}}> Cat </h1>
-                    {/* <img style={{ marginLeft: "30px" }}
-                        src={require("../component/img/강아지_c.png")} alt="dog2" />
-                    <img style={{ marginLeft: "30px" }}
-                        src={require("../component/img/고양이_c.png")} alt="cat2" />
-                    */}
-
+                <ul className='ul1'>
+                    <li> Dog </li>
+                    <li  className='tab' > Cat </li>
                 </ul>
 
             </nav>
             <nav>
-                <ul style={{
-                    display: "flex",
-                    listStyle: "none",
-                    color: "#3B6EAE",
-                    fontSize: "20px",
-                    float: "right"
-                }}>
-                    <a href='/loginminji' style={{ color:"#3B6EAE", marginLeft: "30px",textDecoration: "none" }}>
+                <ul className='ul2'>
+                    <a href='/loginminji'>
                         로그인
                     </a>
-                    <a href='/Join' style={{ color:"#3B6EAE", marginLeft: "30px",textDecoration: "none" }}>
+                    <a href='/Join'>
                         회원가입
                     </a>
-                    <a style={{ marginLeft: "30px", marginRight: "20px" }}>
+                    <a>
                         주문조회
                     </a>
                 </ul>
--- front-end/full/src/page/mainpage/Main.jsx
+++ front-end/full/src/page/mainpage/Main.jsx
@@ -2,60 +2,37 @@ import React from 'react'
 import Header from '../Header'
 import Footer from '../Footer'
 import '../font/font.css'
-
+import './Main.css'
 
 
 const Main = () => {
   return (
-    <div style={{ 
-      backgroundColor: "#F7F3E7", 
-      height: "15vh", 
-      display: "flex", 
-      flexDirection: "column", 
-      fontFamily: "BMJUA_ttf", 
-      color: "#3B6EAE"}}>
+    <div className='body'>
       <Header />
-      <div style={{
-        fontFamily: "BMJUA_ttf",
-        display: "inline-block",
-        float: "right",
-        fontSize: "2rem",
-        marginTop: "5vh",
-        marginBottom:"5vh",
-        textAlign: "center"
-      }}> 댕냥이 쇼핑몰에 오신 것을 환영합니다. 
+      <div className='welcome'> 
+        댕냥이 쇼핑몰에 오신 것을 환영합니다. 
       </div>
 
-
-      <span style={{
-        marginTop:"2.2rem",
-        textAlign: "center",
-        color: "#3B6EAE"
-      }} >
-        <div style={{
-            height: "40vh", 
-            width:"40vw",
-            display:"inline-block"
-          }}>
+      <span className='icon'>
+      <a href='/ProductList2'>
+        <div className='icon1'>
           <h1>
             댕댕 용품
           </h1>
-          <img style={{height:"40vh"}}
-            src={require("../Img/강아지_c.png")} alt="dog2" />
+          <img className='dangImg' 
+            src={require("../Img/강아지_c.png")} alt="dog2" />  
         </div>
-        <div style={{
-            height: "40vh",
-            float:"right",
-            marginRight:"25rem"
-          }}>
+        </a>
+        <a href='/ProductList2'>
+        <div className='icon2'>
+          
           <h1>
             냥냥 용품
           </h1>
-          <img style={{
-            height: "40vh"
-          }}
+          <img className='catImg'
             src={require("../Img/고양이_c.png")} alt="cat2" />
-        </div>
+        
+        </div></a>
       </span>
       <Footer />
     </div>
